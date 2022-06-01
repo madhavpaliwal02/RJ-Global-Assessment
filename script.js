233200aa0156aea771c71d3f82822b173bbd2eab
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-06-02 00:28:02 +0530

    Creating Home & Login page successfully

---
+++ script.js
@@ -0,0 +1,9 @@
+const log_in = document.querySelector('#log-in');
+const sign_up = document.querySelector('#sign_up');
+
+console.log(log_in);
+console.log(sign_up);
+
+// log_in.addEventListener('click', () => {
+
+// })
